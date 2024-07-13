88b5e8e250fc2a2234f8e418beb1863707da4486
Zainab Mustufa <63682505+zabi66@users.noreply.github.com>
2024-07-13 01:40:09 +0100

    Add files via upload

---
+++ second-porfile.js
@@ -0,0 +1,28 @@
+ let slider = tns({
+     container : ".my-tslider",
+     "slideBy" : "1",
+     "speed" : 400,
+     "nav" : false,
+     autoplay : true,
+     controls: false,
+     autoplayButtonOutput : false,
+     responsive : { 
+         1600 : {
+             items : 4,
+             gutter : 20
+         },
+         1024: {
+             items : 3,
+             gutter : 20
+         },
+         768: {
+             items : 2,
+             gutter: 20
+         },
+         480: {
+             items : 1
+         }
+     }
+
+ })
+
